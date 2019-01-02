115c1d87e2539abcb75be14becda3635e2bdc727
阿小庆 <1756260160@qq.com>
2019-01-02 23:01:36 +0800

    update

--- mnist/mnist_testdemo/mnist/convolutional.py
+++ mnist/mnist_testdemo/mnist/convolutional.py
@@ -1,8 +1,8 @@
 import os
 import model
 import tensorflow as tf
-import input_data
-
+# import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 data = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # 定义模型
